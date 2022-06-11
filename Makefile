e1aa67f1236abe36202bca056690024eb3996246
TreshUp <tr@ex.com>
2022-06-11 21:29:33 +0500

    Updated make, first try

--- Makefile
+++ Makefile
@@ -1,7 +1,8 @@
 .DEFAULT_GOAL = install
 .SILENT: install clean
 
-ex_name = sports_notifications@tr.gmail.com
+# TODO parse uuid
+ex_name = sports_notifications@tr.com
 mkfile_path = $(abspath $(lastword $(MAKEFILE_LIST)))
 mkfile_dir = $(dir $(mkfile_path))
 link_path = ~/.local/share/gnome-shell/extensions/$(ex_name)
@@ -14,7 +15,11 @@ install:
 	fi
 
 clean:
-	unlink $(link_path)
-
+	if [ -L $(link_path) ]; then \
+		unlink $(link_path); \
+	else \
+		echo "Doesn't exist"; \
+	fi
+	
 
 .PHONY: install clean
--- src/extension.js
+++ src/extension.js
@@ -1,1 +1,102 @@
-// TODO license
+const Main = imports.ui.main;
+const St = imports.gi.St;
+const GObject = imports.gi.GObject;
+const Gio = imports.gi.Gio;
+const Gtk = imports.gi.Gtk;
+const PanelMenu = imports.ui.panelMenu;
+const PopupMenu = imports.ui.popupMenu;
+const Me = imports.misc.extensionUtils.getCurrentExtension();
+
+let myPopup;
+
+const MyPopup = GObject.registerClass(
+class MyPopup extends PanelMenu.Button {
+
+  _init () {
+  
+    super._init(0);
+    
+    // Creation of base class with logo
+
+    let icon = new St.Icon({
+    //   icon_name : 'security-low-symbolic',
+      gicon : Gio.icon_new_for_string( Me.dir.get_path() + '/sports.svg' ),
+      style_class : 'system-status-icon',
+    });
+   
+    this.add_child(icon);
+
+    
+    
+    // this.menu.addMenuItem( new PopupMenu.PopupSeparatorMenuItem() );a
+    
+    this.menu.connect('open-state-changed', (menu, open) => {
+      if (open) {
+        log('opened');
+      } else {
+        log('closed');
+      }
+    });
+    
+    // image item
+    let popupImageMenuItem = new PopupMenu.PopupImageMenuItem(
+      'Menu Item with Icon',
+      'security-high-symbolic',
+    );
+    this.menu.addMenuItem(popupImageMenuItem);
+
+    // Layout creation
+    this.box_content = new St.BoxLayout({ style_class: 'timepp-content-box', vertical: true});
+    
+    // ??
+    this.box_content._delegate = this;
+
+    let prefsButton = this.createButton('preferences-system-symbolic', _("Weather Settings"));
+    prefsButton.connect('clicked', this.my_test_func.bind(this));
+    this.box_content.add_actor(prefsButton);
+
+    this.menu.box.add_child(this.box_content);
+  
+    
+    // you can close, open and toggle the menu with
+    // this.menu.close();
+    // this.menu.open();
+    // this.menu.toggle();
+  }
+
+  createButton(iconName, accessibleName) {
+    let button;
+
+    button = new St.Button({
+        reactive: true,
+        can_focus: true,
+        track_hover: true,
+        accessible_name: accessibleName,
+        //style_class: 'message-list-clear-button button openweather-button-action'
+    });
+
+    button.child = new St.Icon({
+        icon_name: iconName
+    });
+
+    return button;
+}
+
+my_test_func(){
+  log('my_test_clicked');
+}
+
+
+});
+
+function init() {
+}
+
+function enable() {
+  myPopup = new MyPopup();
+  Main.panel.addToStatusArea('myPopup', myPopup, 1);
+}
+
+function disable() {
+  myPopup.destroy();
+}
