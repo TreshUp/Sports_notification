9422ec8b8ce1f5f6b0d6e85620523c064c80eda9
TreshUp <tr@ex.com>
2022-07-09 22:16:50 +0500

    Fill in prefs

--- src/prefs.js
+++ src/prefs.js
@@ -1,9 +1,9 @@
-const {Gtk, GObject} = imports.gi;
+const {Gtk, GObject, Gio} = imports.gi;
 const Me = imports.misc.extensionUtils.getCurrentExtension();
 
-const MyBuilderScope = GObject.registerClass({
+const SportsScope = GObject.registerClass({
     Implements: [Gtk.BuilderScope],
-}, class MyBuilderScope extends GObject.Object {
+}, class SportsScope extends GObject.Object {
 
     vfunc_create_closure(builder, handlerName, flags, connectObject) {
         if (flags & Gtk.BuilderClosureFlags.SWAPPED)
@@ -15,7 +15,7 @@ const MyBuilderScope = GObject.registerClass({
         return this[handlerName].bind(connectObject || this);
     }
     
-    on_btn_click(connectObject) {
+    on_but_add_clicked(connectObject) {
         connectObject.set_label("Clicked");
     }
 });
@@ -24,10 +24,65 @@ function init () {}
 
 function buildPrefsWidget () {
 
-    let builder = new Gtk.Builder();
+    {
+        let GioSSS = Gio.SettingsSchemaSource;
+        let schemaSource = GioSSS.new_from_directory(
+            Me.dir.get_child("schemas").get_path(),
+            GioSSS.get_default(),
+            false
+          );
+        let schemaObj = schemaSource.lookup(
+            'org.gnome.shell.extensions.sportsnotifications', true);
 
-    builder.set_scope(new MyBuilderScope());
+        this.settings = new Gio.Settings({ settings_schema: schemaObj });
+    }
+
+    this.builder = new Gtk.Builder();
+
+    this.builder.set_scope(new SportsScope());
     //builder.set_translation_domain('gettext-domain');
-    builder.add_from_file(Me.dir.get_path() + '/gtk4.ui');
-    return builder.get_object('settings_page');
+    this.builder.add_from_file(Me.dir.get_path() + '/gtk4.ui');
+    this.bind_settings();
+    return this.builder.get_object('settings_page');
+}
+
+function bind_settings () {
+    let tree_model = this.builder.get_object('list_sports');
+
+    let arr = this.settings.get_value('array-of-sports');
+    const variant_arr = arr.deepUnpack();
+    let arr_length = Object.keys(variant_arr).length;
+    
+    let code = false, f_path = null;
+    if (arr_length != 0)
+    {
+        tree_model.append();
+        [code, f_path] = tree_model.get_iter_first();
+    }
+
+    for (let idx = 0; idx < arr_length; idx++)
+    {
+        //log("Vals:" + Object.values(variant_arr[idx])[3].get_type_string());
+        let g_array = variant_basic_types(Object.values(variant_arr[idx]));
+        tree_model.set(f_path, [...Array(g_array.length).keys()], g_array);
+    }
+
+}
+
+function variant_basic_types(array_of_variants) {
+    let map_lamdas = new Map();
+    map_lamdas.set("s", (element) => { return element.get_string(); });
+    map_lamdas.set("i", (element) => { return element.get_int32(); });
+
+    let array_of_types = array_of_variants.map((element) => {
+        let type = element.get_type_string();
+        let val = map_lamdas.get(type)(element);
+        // Because get_string returns string and its length
+        if (typeof val === 'object')
+            return val[0];
+        else
+            return val;
+      });
+
+    return array_of_types;
 }
