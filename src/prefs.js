534a929768983a45a535b24d48b5d5bd76227d67
TreshUp <tr@ex.com>
2022-10-16 00:03:07 +0500

    Almost have done edit function

--- src/prefs.js
+++ src/prefs.js
@@ -25,9 +25,51 @@ const SportsScope = GObject.registerClass({
         dialog.show();
         // TODO reopen bag
     }
+
+    /**
+     * Function for fill in data from selected row (from global tree)
+     * @param  {Gio.Settings} settings      Settings for schema
+     * @param  {Array} objects_array        Array of ui objects
+     */
+    on_but_edit_clicked(settings, objects_array, button) {
+        // Get all necessary objects
+        let [widget, tree_view, sports_cmb, league_cmb, spin_delta, text_api] = objects_array;
+
+        let selection = tree_view.get_selection();
+        selection.set_mode(Gtk.SelectionMode.BROWSE);
+        let [flag, model, iter] = tree_view.get_selection().get_selected();
+        if (flag) {
+            
+            // Get map from schema
+            let arr = settings.get_value('array-of-sports');
+            let index_of_row = model.get_path(iter).get_indices();
+            // Convert map into array
+            const variant_arr = [...Object.values(arr.deepUnpack()[index_of_row])];
+
+            // Sport and league were set by index
+            sports_cmb.set_active(variant_arr[0]);
+            league_cmb.set_active(variant_arr[1]);
+            // Delta
+            // TODO check
+            spin_delta.set_value(variant_arr[3]);
+            // Api
+            let api = variant_arr[5].get_string();
+            text_api.get_buffer().set_text(api[0], api[1]);
+
+            let dialog = dialog_creation(widget);
+            dialog.show();
+
+            // TODO save changes after ?? (close or ok button)
+        }
+        // todo exception
+    }
     
+    /**
+     * Function which updates list of leagues for selected sport
+     * @param  {Gtk.ComboBox} sports_cmb    Combo of sports
+     * @param  {Gtk.ComboBox} league_cmb    Combo of leagues
+     */
     on_sports_changed(sports_cmb, league_cmb, button) {
-        log("Type1:" + league_cmb.get_model());
         let model = sports_cmb.get_model();
         let [success, iter] = sports_cmb.get_active_iter();
         // todo exception
@@ -66,11 +108,11 @@ function buildPrefsWidget () {
     //builder.set_translation_domain('gettext-domain');
     this.builder.add_from_file(Me.dir.get_path() + '/gtk4.ui');
     this.bind_settings();
-    return this.builder.get_object('settings_page');
+    return ui_objects_getter('settings_page');
 }
 
 function bind_settings () {
-    let tree_model = this.builder.get_object('list_sports');
+    let tree_model = ui_objects_getter('list_sports');
 
     let arr = this.settings.get_value('array-of-sports');
     const variant_arr = arr.deepUnpack();
@@ -83,14 +125,37 @@ function bind_settings () {
     }
 
     fill_in_combos();
-
-    let but_add = this.builder.get_object('but_add');
-    let scope = this.builder.get_scope();
-    but_add.connect('clicked', scope.on_but_add_clicked.bind(this, 
-        this.builder.get_object('view_base_settings')));
+    connect_signals();
 }
 
+function connect_signals() {
+    let scope = this.builder.get_scope();
+    
+    let but_add = ui_objects_getter('but_add');
+    but_add.connect('clicked', scope.on_but_add_clicked.bind(this, ui_objects_getter('view_base_settings')));
+    
+    sports_combo = ui_objects_getter('cmb_sports');
+    sports_combo.connect('changed', scope.on_sports_changed.bind(this, sports_combo, ui_objects_getter('cmb_league')));
+
+    let but_edit = ui_objects_getter('but_edit');
+    let objects_arr = [
+        ui_objects_getter('view_base_settings'), 
+        ui_objects_getter('tree_sports'),
+        sports_combo,
+        ui_objects_getter('cmb_league'),
+        ui_objects_getter('spin_delta'),
+        ui_objects_getter('text_api')
+    ];
+    but_edit.connect('clicked', scope.on_but_edit_clicked.bind(this, this.settings, objects_arr));
+}
+
+/**
+ * Function which converts Vatiants into ints and strings
+ * @param  {Map of Gtk.Variants} array_of_variants    Single object from schema
+ */
 function variant_basic_types(array_of_variants) {
+    // A little hack for converting variants to strings and ints
+    // upd can be done with recursiveUnpack instead deepUnpack
     let map_lamdas = new Map();
     map_lamdas.set("s", (element) => { return element.get_string(); });
     map_lamdas.set("i", (element) => { return element.get_int32(); });
@@ -105,6 +170,7 @@ function variant_basic_types(array_of_variants) {
             return val;
     });
     
+    // Because of index system of db we need to get values from json
     let tmp_f = Object.keys(json_data)[array_of_types[0]];
     array_of_types[0] = tmp_f;
     
@@ -113,6 +179,11 @@ function variant_basic_types(array_of_variants) {
     return array_of_types;
 }
 
+/**
+ * Function which creates model, sets type and render
+ * @param  {Gtk.ComboBox} combo     Current combobox
+ * @param  {GObject} cmb_type       Type of objects in this combo
+ */
 function prepare_combo(combo, cmb_type) {
     let tree_combo = new Gtk.ListStore();
     tree_combo.set_column_types([cmb_type]);
@@ -124,20 +195,21 @@ function prepare_combo(combo, cmb_type) {
     return tree_combo;
 }
 
+/**
+ * Function for filling in all comboboxes - combo with leagues is filled after
+ * selection of sport
+ */
 function fill_in_combos() {
     // log("Type2:" + Object.keys(json_data));
-    sports_combo = this.builder.get_object('cmb_sports');
+    sports_combo = ui_objects_getter('cmb_sports');
     tree_sports_combo = prepare_combo(sports_combo, GObject.TYPE_STRING);
     for (let item of Object.keys(json_data)) {
         tree_sports_combo.set(tree_sports_combo.append(), [0], [item]);    
     }      
     
     // TODO exceptions
-    league_combo = this.builder.get_object('cmb_league');
+    league_combo = ui_objects_getter('cmb_league');
     tree_league_combo = prepare_combo(league_combo, GObject.TYPE_STRING);
-    let scope = this.builder.get_scope();
-    sports_combo.connect('changed', scope.on_sports_changed.bind(this, 
-        sports_combo, league_combo));
 }
 
 function dialog_creation(widget) {
@@ -156,4 +228,11 @@ function dialog_creation(widget) {
         dialog.destroy();
     });
     return dialog;
+}
+/**
+ * Get object from builder by name
+ * @param  {String} name    Name of object
+ */
+function ui_objects_getter(name) {
+    return this.builder.get_object(name)
 }
